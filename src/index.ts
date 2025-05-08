065b8ebc766aedccae8fa028ed59ce1c50f48888
Md Iftekharul Alam <68458610+iftekhar08019@users.noreply.github.com>
2025-05-08 11:48:10 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+function formatString(input: string, toUpper?: boolean): string {
+  if (toUpper === false) {
+    return input.toLowerCase();
+  } else {
+    return input.toUpperCase();
+  }
+}
+
+console.log(formatString("Hello"))         // Output: "HELLO"
+console.log(formatString("Hello", true))   // Output: "HELLO"
+console.log(formatString("Hello", false));  // Output: "hello"
